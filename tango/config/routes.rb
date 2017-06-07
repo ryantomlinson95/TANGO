1d3830d44ab18f4677184d083a5bcf315329924e
Kayli Doll <doll.64@osu.edu>
2017-06-07 14:18:39 -0400

    test2

--- tango/config/routes.rb
+++ tango/config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   resources :projects
+
+  get '/about', to 'static_pages#about'
+
   devise_for :users
   devise_for :views
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-root "static_pages#home"
+
+  root "static_pages#home"
+
+
+
 
 end
