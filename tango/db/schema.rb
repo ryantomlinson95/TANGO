145ba3a2a5a3dc8a2e6c384bec2e8ff150d9107e
ryantomlinson95 <ryantomlinson95@gmail.com>
2017-06-07 12:11:48 -0400

    Skeleton pushed

--- tango/db/schema.rb
+++ tango/db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170607144315) do
+ActiveRecord::Schema.define(version: 20170607155440) do
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "githuburl"
+    t.integer "progress"
+    t.string "owner"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
